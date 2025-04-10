39aba3c340a57831357d2617800f6ff2e19cd0eb
thematei <matei.milotoiu@gmail.com>
2025-04-10 21:40:35 +0300

    moved tests file; refined first test

--- __tests__/plugin.test.js
+++ __tests__/plugin.test.js
@@ -1,28 +1,32 @@
-import { jest } from "@jest/globals"
-import { mockApp, mockPlugin, mockNote } from "../test-helpers.js"
+import { mockApp, mockPlugin, mockNote } from "../lib/test-helpers.js"
 
 const REPO_URL = "https://github.com/alloy-org/plugin-builder";
 // --------------------------------------------------------------------------------------
-describe("This here plugin", () => {
+describe("This here plugin,", () => {
   const plugin = mockPlugin();
   plugin._testEnvironment = true;
 
   // --------------------------------------------------------------------------------------
-  describe("With a shift-click on a day", () => {
+  describe("with a shift-click on a day,", () => {
+
+    var app;
 
     beforeEach(() => {
-      const app = mockApp();
+      app = mockApp();
     });
 
+    //   Shift-navigating to note: name reflects day correctly; tags are being saved properly;
     it("should create a new note with selected tags", async () => {
       //set up environment
-      const date = {};
-      const dayName = "";
+      const date = new Date("March 31 2025");
+      const dayName = "March 31st, 2025";
       //make sure my plugin works with the selected tag
       const selectedTag = "daily-jots";
       const jotCount = plugin._state.jots.length;
+      const monthNames = plugin._constants.monthNames;
       //load all notes from app
-      const notes = await app.filterNotes({tag: selectedTag});
+      app.settings[plugin._constants.settings.TAGS] = selectedTag;
+      let notes = await app.filterNotes({tag: selectedTag});
       //call plugin
       await plugin.onEmbedCall(app, "navigate", date, true, dayName);
 
@@ -33,21 +37,18 @@ describe("This here plugin", () => {
       expect(plugin._state.jots.length).toBe(jotCount + 1);
       let newJot = plugin._state.jots.at(plugin._state.jots.length - 1);
       expect(newJot.day).toBe(date.getDate());
-      expect(newJot.month).toBe(date.getMonth());
+      expect(newJot.month).toBe(monthNames[date.getMonth()]);
       expect(newJot.year).toBe(date.getFullYear());
       //test that a real note was actually created in app
         //compare with initially loaded notes
-      const newlyCreatedNote = newNotes.filter((newNote) => {
-        !notes.contains((note)=>(note.name === newNote.name, note.uuid === newNote.uuid));
+      const newlyCreatedNote = newNotes.find((newNote) => {
+        return !notes.some((note)=>(note.uuid === newNote.uuid && note.name === newNote.name));
       });
       expect(newlyCreatedNote.uuid).toBe(newJot.uuid);
       //test that uuid of new note matches uuid of jot
       await app.findNote({uuid: newJot.uuid});
     });
   });
-  //   Shift-navigating to note
-  // name reflects day correctly
-  // tags are being saved properly
 
   describe("with an existing daily jot", () => {
   beforeEach(() => {
